403ec8a4de7e14cc78adfcbb36b457db23fb5d2c
Webster Diogenes <web.driw12@gmail.com>
2025-11-10 01:19:48 -0300

    cards API query

--- msavaliadorcredito/src/main/java/io/github/websterrodrigues/msavaliadorcredito/resource/CardResourceClient.java
+++ msavaliadorcredito/src/main/java/io/github/websterrodrigues/msavaliadorcredito/resource/CardResourceClient.java
@@ -1,4 +1,16 @@
 package io.github.websterrodrigues.msavaliadorcredito.resource;
 
+import io.github.websterrodrigues.msavaliadorcredito.model.ClientCard;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@FeignClient(value = "mscartoes", path = "/cards")
 public interface CardResourceClient {
+
+    @GetMapping(params = "cpf")
+    ResponseEntity<List<ClientCard>> getCardsByClient(@RequestParam String cpf);
 }
--- msavaliadorcredito/src/main/java/io/github/websterrodrigues/msavaliadorcredito/service/CreditAppraiserService.java
+++ msavaliadorcredito/src/main/java/io/github/websterrodrigues/msavaliadorcredito/service/CreditAppraiserService.java
@@ -1,11 +1,16 @@
 package io.github.websterrodrigues.msavaliadorcredito.service;
 
+import io.github.websterrodrigues.msavaliadorcredito.model.ClientCard;
+import io.github.websterrodrigues.msavaliadorcredito.resource.CardResourceClient;
 import io.github.websterrodrigues.msavaliadorcredito.resource.ClientResourceClient;
 import io.github.websterrodrigues.msavaliadorcredito.model.CustomerSituation;
 import io.github.websterrodrigues.msavaliadorcredito.model.CustumerData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Service
 public class CreditAppraiserService {
@@ -13,15 +18,21 @@ public class CreditAppraiserService {
     @Autowired
     private ClientResourceClient clientResourceClient;
 
+    @Autowired
+    private CardResourceClient cardResourceClient;
+
     public CustomerSituation getCustomerSituation(String cpf) {
         //obter dados do cliente - msclientes
         ResponseEntity<CustumerData> custumerDataResponse = clientResourceClient.getByCpf(cpf);
+        //obter cartoes do cliente - mscartoes
+        ResponseEntity<List<ClientCard>> cardResponse = cardResourceClient.getCardsByClient(cpf);
 
         return CustomerSituation.builder()
                 .customerData(custumerDataResponse.getBody())
+                .cards(cardResponse.getBody())
                 .build();
 
 
-        //obter cartoes do cliente - mscartoes
+
     }
 }
